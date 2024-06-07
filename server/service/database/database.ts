d5a329314784f5179dd55abaa611714bd5cc8b12
alexismrosales <aleisjorge91@gmail.com>
2024-06-07 00:25:21 -0600

    Parametrized query function created

--- server/service/database/database.ts
+++ server/service/database/database.ts
@@ -1,7 +1,7 @@
-import { connect, ConnectionPool, config } from 'mssql'
+import { connect, ConnectionPool, config, NVarChar } from 'mssql'
 import { credentials } from './credentials'
 
-export default class Database {
+class Database {
   private credentials: config
   private poolConnection: Promise<ConnectionPool>
 
@@ -13,6 +13,7 @@ export default class Database {
   private async establishConn() {
     try {
       const poolConnection = await connect(this.credentials)
+
       console.log("Connection stablished")
       return poolConnection
     } catch (error) {
@@ -20,13 +21,30 @@ export default class Database {
     }
   }
 
+  // Executing a simple query in database
   public async execQuery(query: string) {
     const conn = await this.poolConnection
     const resultSet = await conn.request().query(query)
+
     conn.close()
     console.log("Query executed succesfully")
     return resultSet.recordset
   }
 
-}
+  // Executing a parametrized query
+  public async execParametrizedQuery(query: string, parameters: any[]) {
+    const conn = await this.poolConnection
+    const request = conn.request()
 
+    // Adding new parameters
+    for (const param of parameters) {
+      request.input(param.name, NVarChar, param.value);
+    }
+
+    const resultSet = await request.query(query)
+    conn.close()
+    console.log("Parametrized query executed succesfully")
+    return resultSet.recordset
+  }
+}
+export default new Database()
