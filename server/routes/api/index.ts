edcb6d20db00e087865ef63455f79a046b5d12b8
alexismrosales <aleisjorge91@gmail.com>
2024-06-07 23:06:31 -0600

    new routes added

--- server/routes/api/index.ts
+++ server/routes/api/index.ts
@@ -1,9 +1,11 @@
 import Router from '../router'
 import AdminRoute from './admin'
+import GroupsRoute from './groups'
 class ApiRoute extends Router {
   public baseRoute = '/api';
   public routes() {
     this.router.use(AdminRoute.baseRoute, AdminRoute.router);
+    this.router.use(GroupsRoute.baseRoute, GroupsRoute.router)
   }
 }
 export default new ApiRoute();
